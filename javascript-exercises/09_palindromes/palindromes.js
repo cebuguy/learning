356f61b124c99310d5de078f2afa373b181f4f90
trungn <tknghiem1@gmail.com>
2022-02-10 10:12:34 -0800

    solution to unit test 09 fibonacci

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //take away spaces/punctuation and make lower case in newString copy
+    let newString = string.replaceAll(' ','').replaceAll('!','').replaceAll('.','').replaceAll(',','').toLowerCase();
+    
+    //split into array
+    let array = newString.split('');
+
+    //reverse array and combine into a string
+    let reverseString = [...array].reverse().join('');
+    
+    if(newString === reverseString) return true;
+    else return false;
 
 };
 
