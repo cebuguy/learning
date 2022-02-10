a05c60270dfb8fd32e7a60f8db6a13ac9fe50b8d
trungn <tknghiem1@gmail.com>
2022-02-09 22:20:50 -0800

    complete solution to unit test 08 calculator

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+  arr.forEach(element => result += element);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  arr.forEach(element => sum *= element);
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let sum = 1;
+  for(let i = num; i > 0; i-- ) {
+    if(num === 0) break;
+    sum *= num;
+    num--;
+    console.log(sum);
+    console.log(i);
+}
+return sum;
 };
 
 // Do not edit below this line
