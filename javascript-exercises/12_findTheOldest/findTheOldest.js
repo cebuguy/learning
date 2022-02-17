31c91183719890835de19a84da1b44c5ae450be8
trungn <tknghiem1@gmail.com>
2022-02-16 20:52:29 -0800

    add solution to unit test 12 find oldest

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort( (previous,current) => {
+        const currentYear = new Date();
+        if(previous.yearOfDeath === undefined) previous.yearOfDeath = currentYear.getFullYear();
+        if(current.yearOfDeath === undefined) current.yearOfDeath = currentYear.getFullYear();
+        return (current.yearOfDeath - current.yearOfBirth) - (previous.yearOfDeath - previous.yearOfBirth)
+    });
+    return array[0];
 };
 
 // Do not edit below this line
